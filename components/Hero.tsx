b5f8c1ef67abf71cedc2043185f78520115bdc60
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-03-12 11:33:00 -0400

    messing around with looks

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10">
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                    Dynamic Web Magic with Next.js
+                    
                 </h2>
                 
                 <TextGenerateEffect 
@@ -31,7 +31,7 @@ const Hero = () => {
                 />
 
                 <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                    Hi, I'm Amin, a Next.js Developer based in Boston
+                    Hi, I'm Amin, a Computer Science student based in Boston
                 </p>
 
                 <a href="#about">
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -32,7 +32,6 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  moreInfo,
   id,
   img,
   imgClassName,
@@ -42,7 +41,6 @@ export const BentoGridItem = ({
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  moreInfo?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
   id?: number;
@@ -69,67 +67,109 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(84,9,121,0.7455357142857143) 35%, rgba(0,212,255,1) 100%)',
       }}
     >
-        <div className={`${id === 6 && 'flex justify-center'} h-full}`}>
-            <div className="w-full h-full absolute">
-                {img && (
-                    <img 
-                        src={img}
-                        alt={img}
-                        className={cn(imgClassName, 'object-cover, object-center')}
-                    />
-                )}
-            </div>
-            <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
-                {spareImg && (
-                    <img 
-                        src={spareImg}
-                        alt={spareImg}
-                        className={'object-cover, object-center w-full h-full'}
-                    />
-                )}
-            </div>
-            {id === 6 && (
-                <BackgroundGradientAnimation>
-                    {/* <div className="absolute z-10 flex items-center justify-center text-white font-bold"/> */}
-                </BackgroundGradientAnimation>
-            )}
+      <div className={`${id === 6 && 'flex justify-center'} h-full}`}>
+          <div className="w-full h-full absolute">
+              {img && (
+                  <img 
+                      src={img}
+                      alt={img}
+                      className={cn(imgClassName, 'object-cover, object-center')}
+                  />
+              )}
+          </div>
 
-            <div className={cn(
-                titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col padding-x-5 lg:p-10 md:pl-10 pl-5 ' )}>
-                    <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 whitespace-pre-line">
-                        {description}
-                    </div>
-                    <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
-                        {title}
-                    </div>
-                    <div  className="font-sans text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 whitespace-pre-line ml-0 mt-4 mb-4  ">
-                      {moreInfo}
-                    </div>
-              {id === 2 && <GlobeDemo />}
+          <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+              {spareImg && (
+                  <img 
+                      src={spareImg}
+                      alt={spareImg}
+                      className={'object-cover, object-center w-full h-full'}
+                  />
+              )}
+          </div>
+          {id === 6 && (
+              <BackgroundGradientAnimation>
+                  {/* <div className="absolute z-10 flex items-center justify-center text-white font-bold"/> */}
+              </BackgroundGradientAnimation>
+          )}
+          <div className={cn(
+              titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col padding-x-5 lg:p-10 md:pl-10 pl-5 ' )}>
 
-              {id === 3 && (
-                <div className="flex gap-1 lg:gap-2 w-fit absolute -top-2 lg:-right-2 mt-4">
-                  <div className="flex flex-col gap-3 lg:gap-5">
-                    {['React.js', 'Next.js', 'TypeScript'].map((item) => (
-                      <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-ceenter bg-[#10132E]">
-                        {item}
-                      </span>
-                    ))}
-                    <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                  <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 whitespace-pre-line">
+                      {description}
                   </div>
-                  <div className="flex flex-col gap-3 lg:gap-5">
-                    <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
-                    {['JavaScript', 'Python', 'Java'].map((item) => (
-                      <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-ceenter bg-[#10132E]">
-                        {item}
-                      </span>
-                    ))}
+                  
+                  <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                      {title}
+                  </div>
+                  
+            
+            {id === 2 && <GlobeDemo />}
+
+            {id === 3 && (
+              <div className="flex gap-1 lg:gap-2 w-fit absolute -top-2 lg:-right-2 mt-4">
+                <div className="flex flex-col gap-3 lg:gap-5">
+                  {['React.js', 'Next.js', 'TypeScript'].map((item) => (
+                    <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-ceenter bg-[#10132E]">
+                      {item}
+                    </span>
+                  ))}
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                </div>
+                <div className="flex flex-col gap-3 lg:gap-5">
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                  {['JavaScript', 'Python', 'Java'].map((item) => (
+                    <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-ceenter bg-[#10132E]">
+                      {item}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+{/* border bg-black-100 */}
+            {id == 5 && (
+              <div className="flex justify-around mb-10"> {/* Container */}
+                <div className=" w-[600px]  -mt-5 mb-10"> {/* School descriptions */}
+                  <div className="flex justify-between mb-5"> {/* NEU */}
+                    <p className="flex items-center text-2xl text-purple">
+                      Northeastern University
+                    </p>
                     
+                    <ul className="list-disc pl-5 ">
+                      <li>2024-2025</li>
+                      <li>GPA: 4.0</li>
+                      <li>NUVR Executive Board Member</li>
+                      <li>Intro to Java</li>
+                    </ul>
+                  </div>
+
+                  <div className="flex justify-between  "> {/* Dif school */}
+                    <p className="flex items-center text-2xl text-purple ">
+                      Another School
+                    </p>
+      
+                    <ul className="list-disc pl-5 ">
+                      <li>2025-Present</li>
+                      <li>GPA: 4.0</li>
+                      <li>NUVR Executive Board Member</li>
+                      <li>Intro to Java</li>
+                    </ul>
                   </div>
                 </div>
-              )}
 
-            
+                <div className="flex flex-wrap border">
+                  <div className="w-1/2 flex justify-center -mt-20">
+                    <img src="/NEUlogo2.png" width="100" height="auto" />
+                  </div>
+                  <div className="w-1/2"/>
+
+                  <div className="w-1/2"/>
+                  <div className="w-1/2 flex justify-center">
+                    <img src="/NEUlogo2.png" width="100" height="auto" />
+                  </div>
+                </div>
+              </div>
+            )}
 
             {id === 6 && (
               <div className="mt-5 relative">
@@ -153,6 +193,7 @@ export const BentoGridItem = ({
                 />
               </div>                
             )}
+
           </div>
       </div>
     </div>
